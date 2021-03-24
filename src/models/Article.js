89364ef00df6ea53c88ac266da4038ca018a7e7a
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-03-24 14:58:02 +0530

    athenticate schema

--- src/models/Article.js
+++ src/models/Article.js
@@ -1,41 +1,50 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 
-const articleSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const articleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    votes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    comments: [
+      {
+        type: Schema.Types.String,
+        ref: "Comment",
+      },
+    ],
   },
-  slug: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  text: {
-    type: String
-  },
-  views: {
-    type: Number,
-    default: 0
-  },
-  votes: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  }],
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  tags: [{
-    type: String,
-  }],
-  published: {
-    type: Boolean,
-    default: false
-  },
-  comments: [{
-    type: Schema.Types.String,
-    ref: 'Comment',
-  }]
-}, { timestamps: true })
+  { timestamps: true }
+);
 
-module.exports = model('Article', articleSchema)
+module.exports = model("Article", articleSchema);
